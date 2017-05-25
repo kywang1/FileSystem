11b96a62015376e9d7618d78a06d95f4b4a32754
Alan Zhao <azhao00@pc4.cs.ucdavis.edu>
2017-05-24 20:12:00 -0700

    unmount/write to disk error?

--- libfs/fs.c
+++ libfs/fs.c
@@ -9,14 +9,15 @@
 
 #define FAT_EOC 0xFFFF
 
-typedef struct root_entry{
+typedef struct __attribute__((__packed__)) root_entry{
 	char filename[16];
 	uint32_t size;
 	uint16_t index;
 }root_entry;
 
 
-typedef struct __attribute__((__packed__)) FAT{
+typedef struct __attribute__((__packed__)) FAT{			// max fat block count is 4 since max block count is 8192
+	uint16_t fat_array[4096];
 	int used;
 }FAT;
 
@@ -30,14 +31,13 @@ typedef struct __attribute__((__packed__)) SB{
 }SB;
 
 typedef struct __attribute__((__packed__)) RD{
+	char test[2816];
 	root_entry *root_array[128];
 	int used;
 }RD;
 
-
-
+FAT* fat[4];
 SB* sb;
-FAT* fat;
 RD* rd;
 
 int fs_mount(const char *diskname)
@@ -49,32 +49,69 @@ int fs_mount(const char *diskname)
 	}
 
 	sb = (SB*)malloc(sizeof(SB));
-	fat = (FAT*)malloc(sizeof(FAT));
 	rd = (RD*)malloc(sizeof(RD));
 
-	fat->used = 1;
-
-
 	block_read(0, (void*)sb);
+	
+	for(int i = 0; i < sb->FAT_blocks; i++)
+	{
+		fat[i] = (FAT*)malloc(sizeof(FAT));
+		block_read(1 + i,(void*)(fat[i]->fat_array));
+	}
 
+	block_read(sb->root_index, (void*)(rd->test));
+
+	for(int i = 0; i < 128; i++)
+	{
+		rd->root_array[i] = (root_entry*)malloc(sizeof(root_entry));
+		memcpy(rd->root_array[i], rd->test + (i * 22), 22);
+	}
+
+	fat[0]->used = 1;
 
 	return 0;
 }
 
 int fs_umount(void)
 {
+/*	if(sb)
+	{
+		free(sb);
+	}
+*/
+	for(int i = 0; i < 4; i++)
+	{
+		if(fat[i])
+		{
+			free(fat[i]);
+		}
+		i++;
+	}
+
+	for(int i = 0; i < 128; i++)
+	{
+		if(rd->root_array[i])
+		{
+			free(rd->root_array[i]);
+		}
+
+		i++;
+	}
+
+//	free(rd);	
+
 	return(block_disk_close());
 }
 
 int fs_info(void)
 {
-	puts("FS Info:");
+	printf("FS Info:\n");
 	printf("total_blk_count=%hu\n",sb->block_total);
 	printf("fat_blk_count=%hu\n", sb->FAT_blocks);
-	puts("rdir_blk=1\n"); 						// come back to this
-	puts("data_blk=1\n"); 						// come back to this
+	printf("rdir_blk=%hu\n", sb->root_index); 						// come back to this
+	printf("data_blk=%hu\n", sb->start_index); 						// come back to this
 	printf("data_blk_count=%hu\n", sb->data_blocks);
-	printf("fat_free_ratio=%hu/%hu\n", sb->data_blocks - fat->used, sb->data_blocks);
+	printf("fat_free_ratio=%hu/%hu\n", (sb->data_blocks - fat[0]->used), sb->data_blocks);
 	printf("rdir_free_ratio=%hu/128\n", 128 - rd->used);
 	return 0;
 }
@@ -92,11 +129,13 @@ int fs_create(const char *filename)
 
 	for(; i < 128; i++)
 	{
-		if(rd->root_array[i] != NULL)
+		if(rd->root_array[i]->filename[0] != 0)
 		{
 			used_count++;
 			if(strcmp(rd->root_array[i]->filename, filename) == 0)
+			{
 				return -1;
+			}
 
 			if(used_count == rd->used)
 			{
@@ -105,7 +144,7 @@ int fs_create(const char *filename)
 		}
 	}
 
-	while(rd->root_array[free_index] != NULL)
+	while(rd->root_array[free_index]->filename[0] != 0)
 	{
 		free_index++;
 
@@ -116,23 +155,61 @@ int fs_create(const char *filename)
 	}
 
 
-	root_entry *new_entry = (root_entry*)malloc(sizeof(root_entry));
-	strcpy(new_entry->filename, filename);
-	new_entry->size = 0;
-	new_entry->index = 0xFFFF;
-	rd->root_array[free_index] = new_entry;
+	rd->root_array[free_index]->size = 0;
+	rd->root_array[free_index]->index = FAT_EOC;
+	strcpy(rd->root_array[free_index]->filename, filename);
+	rd->used++;
+
 
 	return 0;
 }
 
 int fs_delete(const char *filename)
 {
+	if(strlen(filename) > 16)
+	{
+		return -1;
+	}
+
+	int i = 0, used_count = 0;
+
+	for(; i < 128; i++)
+	{
+		if(rd->root_array[i]->filename[0] != 0)
+		{
+			if(strcmp(rd->root_array[i]->filename, filename) == 0)
+			{
+				memset(rd->root_array[i]->filename, 0, 16);
+				rd->used--;
+				return 0;
+			}
+
+			if(used_count == rd->used)
+			{
+				break;
+			}
+		}
+	}
+
 	return 0;
 }
 
 int fs_ls(void)
 {
-	printf("ls");
+	int used = 0;
+	printf("FS Ls:\n");
+	for(int i = 0; i < 128; i++)
+	{
+		if(rd->root_array[i]->filename[0] != 0)
+		{
+			printf("file: %s, size: %d, data_blk: %d\n", rd->root_array[i]->filename, rd->root_array[i]->size, rd->root_array[i]->index);
+			used++;
+		}
+		if(used == rd->used)
+		{
+			break;
+		}
+	}
 	return 0;
 }
 
